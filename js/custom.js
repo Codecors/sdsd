735ad71204e81e5f7afe707948e7b0a26a09a430
Ken Zhao <zhaohaozhou@gmail.com>
2015-04-08 00:26:31 -0700

    Page content switching works now. Replaced with .SlideToggle jQuery manipulation instead of use .slideup & .slidedown separately. There is some style issue to be fixed later.

--- js/custom.js
+++ js/custom.js
@@ -2,29 +2,22 @@
  * Created by kenneth on 4/5/15.
  */
 $(document).ready(function() {
+    $('.jqueryOptions').hide();
+    $('div#homepage').slideDown();
 
-    $("#switch li").click(function() {
-        //  First remove class "active" from currently active tab
-     //   $("#nav ul li").removeClass('active');
-
-        //  Now add class "active" to the selected/clicked tab
-    //    $(this).addClass("active");
-        //  Hide all tab content
-        $(".page-content").hide();
-
-        //  Here we get the href value of the selected tab
-        var selected_page = $(this).find("a").attr("href");
-
-
-        //  Show the selected tab content
-        $(selected_page).fadeIn();
-
-        //  At the end, we add return false so that the click on the link is not executed
-        return false;
+    $('.clickme').click(function () {
+        //$('.jqueryOptions').removeClass('current-opt');
+        $('.jqueryOptions').hide();
+        var myid = this.id;
+        $("div"+"#"+myid).slideToggle();
+        //$("." + $(this).val()).addClass('current-opt');
     });
 
     $(".toggle_title").click(function() {
         $(this).toggleClass('toggle_active');
         $(this).siblings('.toggle_content').slideToggle("fast");
     });
+});
+$(function() {
+
 });
