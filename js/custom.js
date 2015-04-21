72ca80994c569e2a9104421a81d3dd35d96e07d0
Ken Zhao <zhaohaozhou@gmail.com>
2015-04-21 00:14:31 -0700

    Redesigned product btns

--- js/custom.js
+++ js/custom.js
@@ -11,14 +11,7 @@ $(document).ready(function() {
         $(this).toggleClass('toggle_active');
         $(this).siblings('.toggle_content').slideToggle("fast");
     });
-    $("#myslider").owlCarousel({
 
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-       // autoHeight: true
-    });
     $("#myproducts").owlCarousel({
         autoPlay: 3000, //Set AutoPlay to 3 seconds
         itemsCustom : [
